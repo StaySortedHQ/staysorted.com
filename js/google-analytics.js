1185e9f93aee3d9f2dcf3028094cbd4d4f6900de
Harry Ng <harryworld@gmail.com>
2017-02-28 17:04:32 +0800

    Update GA Tracking ID

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -3,6 +3,6 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-68153845-1', 'auto');
+ga('create', 'UA-92763723-1', 'auto');
 ga('send', 'pageview');
 
